c60a1687dae8257dc72fdd28bc55d8b5c835eabf
gadok <gado.krisztian@montana.hu>
2021-06-07 12:00:14 +0200

    femm_warrpers updated

--- examples/femm_wrapper/femm_current_flow_problem.py
+++ examples/femm_wrapper/femm_current_flow_problem.py
@@ -26,74 +26,74 @@ J = Ig / (depth * 1e-3 * ht * 1e-3)
 def current_flow_problem():
     writer = FemmWriter()
     writer.field = kw_current_flow
-    writer.lua_model.extend(writer.init_problem("current_data.csv"))
+    writer.init_problem("current_data.csv")
 
-    writer.lua_model.append(writer.currentflow_problem("millimeters", "planar", depth=depth))
+    writer.currentflow_problem("millimeters", "planar", depth=depth)
 
     # Geometry points
-    writer.lua_model.append(writer.add_node(-wcm / 2, hc / 2))
-    writer.lua_model.append(writer.add_node(wcm / 2, hc / 2))
-    writer.lua_model.append(writer.add_node(-wcm / 2, -hc / 2))
-    writer.lua_model.append(writer.add_node(wcm / 2, -hc / 2))
+    writer.add_node(-wcm / 2, hc / 2)
+    writer.add_node(wcm / 2, hc / 2)
+    writer.add_node(-wcm / 2, -hc / 2)
+    writer.add_node(wcm / 2, -hc / 2)
 
-    writer.lua_model.append(writer.add_node(-wcm / 2, hc / 2 + hcm))
-    writer.lua_model.append(writer.add_node(wcm / 2, hc / 2 + hcm))
-    writer.lua_model.append(writer.add_node(-wcm / 2, -hc / 2 - hcm))
-    writer.lua_model.append(writer.add_node(wcm / 2, -hc / 2 - hcm))
+    writer.add_node(-wcm / 2, hc / 2 + hcm)
+    writer.add_node(wcm / 2, hc / 2 + hcm)
+    writer.add_node(-wcm / 2, -hc / 2 - hcm)
+    writer.add_node(wcm / 2, -hc / 2 - hcm)
 
-    writer.lua_model.append(writer.add_node(-wt / 2, ht / 2))
-    writer.lua_model.append(writer.add_node(wt / 2, ht / 2))
-    writer.lua_model.append(writer.add_node(-wt / 2, -ht / 2))
-    writer.lua_model.append(writer.add_node(wt / 2, -ht / 2))
+    writer.add_node(-wt / 2, ht / 2)
+    writer.add_node(wt / 2, ht / 2)
+    writer.add_node(-wt / 2, -ht / 2)
+    writer.add_node(wt / 2, -ht / 2)
 
     # Vertical line segments
-    writer.lua_model.append(writer.add_segment(-wt / 2, ht / 2, -wt / 2, -ht / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, ht / 2, -wcm / 2, hc / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, ht / 2, -wcm / 2, -hc / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, -hc / 2, -wcm / 2, -ht / 2))
+    writer.add_segment(-wt / 2, ht / 2, -wt / 2, -ht / 2)
+    writer.add_segment(-wcm / 2, ht / 2, -wcm / 2, hc / 2)
+    writer.add_segment(-wcm / 2, ht / 2, -wcm / 2, -hc / 2)
+    writer.add_segment(-wcm / 2, -hc / 2, -wcm / 2, -ht / 2)
 
-    writer.lua_model.append(writer.add_segment(wt / 2, ht / 2, wt / 2, -ht / 2))
-    writer.lua_model.append(writer.add_segment(wcm / 2, ht / 2, wcm / 2, hc / 2))
-    writer.lua_model.append(writer.add_segment(wcm / 2, ht / 2, wcm / 2, -hc / 2))
-    writer.lua_model.append(writer.add_segment(wcm / 2, -hc / 2, wcm / 2, -ht / 2))
+    writer.add_segment(wt / 2, ht / 2, wt / 2, -ht / 2)
+    writer.add_segment(wcm / 2, ht / 2, wcm / 2, hc / 2)
+    writer.add_segment(wcm / 2, ht / 2, wcm / 2, -hc / 2)
+    writer.add_segment(wcm / 2, -hc / 2, wcm / 2, -ht / 2)
 
     # Horizontal line segments
-    writer.lua_model.append(writer.add_segment(-wt / 2, ht / 2, -wcm / 2, ht / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, ht / 2, wcm / 2, ht / 2))
-    writer.lua_model.append(writer.add_segment(wcm / 2, ht / 2, wt / 2, ht / 2))
+    writer.add_segment(-wt / 2, ht / 2, -wcm / 2, ht / 2)
+    writer.add_segment(-wcm / 2, ht / 2, wcm / 2, ht / 2)
+    writer.add_segment(wcm / 2, ht / 2, wt / 2, ht / 2)
 
-    writer.lua_model.append(writer.add_segment(-wt / 2, -ht / 2, -wcm / 2, -ht / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, -ht / 2, wcm / 2, -ht / 2))
-    writer.lua_model.append(writer.add_segment(wcm / 2, -ht / 2, wt / 2, -ht / 2))
+    writer.add_segment(-wt / 2, -ht / 2, -wcm / 2, -ht / 2)
+    writer.add_segment(-wcm / 2, -ht / 2, wcm / 2, -ht / 2)
+    writer.add_segment(wcm / 2, -ht / 2, wt / 2, -ht / 2)
 
-    writer.lua_model.append(writer.add_segment(-wcm / 2, hc / 2, wcm / 2, hc / 2))
-    writer.lua_model.append(writer.add_segment(-wcm / 2, -hc / 2, wcm / 2, -hc / 2))
+    writer.add_segment(-wcm / 2, hc / 2, wcm / 2, hc / 2)
+    writer.add_segment(-wcm / 2, -hc / 2, wcm / 2, -hc / 2)
 
     # Circle points
-    writer.lua_model.append(writer.add_node(-wk / 2, hk / 2 + dk / 2))
-    writer.lua_model.append(writer.add_node(-wk / 2 - dk, hk / 2 + dk / 2))
+    writer.add_node(-wk / 2, hk / 2 + dk / 2)
+    writer.add_node(-wk / 2 - dk, hk / 2 + dk / 2)
 
-    writer.lua_model.append(writer.add_node(-wk / 2, -hk / 2 - dk / 2))
-    writer.lua_model.append(writer.add_node(-wk / 2 - dk, -hk / 2 - dk / 2))
+    writer.add_node(-wk / 2, -hk / 2 - dk / 2)
+    writer.add_node(-wk / 2 - dk, -hk / 2 - dk / 2)
 
-    writer.lua_model.append(writer.add_node(wk / 2, hk / 2 + dk / 2))
-    writer.lua_model.append(writer.add_node(wk / 2 + dk, hk / 2 + dk / 2))
+    writer.add_node(wk / 2, hk / 2 + dk / 2)
+    writer.add_node(wk / 2 + dk, hk / 2 + dk / 2)
 
-    writer.lua_model.append(writer.add_node(wk / 2, -hk / 2 - dk / 2))
-    writer.lua_model.append(writer.add_node(wk / 2 + dk, -hk / 2 - dk / 2))
+    writer.add_node(wk / 2, -hk / 2 - dk / 2)
+    writer.add_node(wk / 2 + dk, -hk / 2 - dk / 2)
 
     # Adding arc segments
-    writer.lua_model.append(writer.add_arc(-wk / 2, hk / 2 + dk / 2, -wk / 2 - dk, hk / 2 + dk / 2, 180, 1))
-    writer.lua_model.append(writer.add_arc(-wk / 2 - dk, hk / 2 + dk / 2, -wk / 2, hk / 2 + dk / 2, 180, 1))
+    writer.add_arc(-wk / 2, hk / 2 + dk / 2, -wk / 2 - dk, hk / 2 + dk / 2, 180, 1)
+    writer.add_arc(-wk / 2 - dk, hk / 2 + dk / 2, -wk / 2, hk / 2 + dk / 2, 180, 1)
 
-    writer.lua_model.append(writer.add_arc(-wk / 2, -hk / 2 - dk / 2, -wk / 2 - dk, -hk / 2 - dk / 2, 180, 1))
-    writer.lua_model.append(writer.add_arc(-wk / 2 - dk, -hk / 2 - dk / 2, -wk / 2, -hk / 2 - dk / 2, 180, 1))
+    writer.add_arc(-wk / 2, -hk / 2 - dk / 2, -wk / 2 - dk, -hk / 2 - dk / 2, 180, 1)
+    writer.add_arc(-wk / 2 - dk, -hk / 2 - dk / 2, -wk / 2, -hk / 2 - dk / 2, 180, 1)
 
-    writer.lua_model.append(writer.add_arc(wk / 2, hk / 2 + dk / 2, wk / 2 + dk, hk / 2 + dk / 2, 180, 1))
-    writer.lua_model.append(writer.add_arc(wk / 2 + dk, hk / 2 + dk / 2, wk / 2, hk / 2 + dk / 2, 180, 1))
+    writer.add_arc(wk / 2, hk / 2 + dk / 2, wk / 2 + dk, hk / 2 + dk / 2, 180, 1)
+    writer.add_arc(wk / 2 + dk, hk / 2 + dk / 2, wk / 2, hk / 2 + dk / 2, 180, 1)
 
-    writer.lua_model.append(writer.add_arc(wk / 2, -hk / 2 - dk / 2, wk / 2 + dk, -hk / 2 - dk / 2, 180, 1))
-    writer.lua_model.append(writer.add_arc(wk / 2 + dk, -hk / 2 - dk / 2, wk / 2, -hk / 2 - dk / 2, 180, 1))
+    writer.add_arc(wk / 2, -hk / 2 - dk / 2, wk / 2 + dk, -hk / 2 - dk / 2, 180, 1)
+    writer.add_arc(wk / 2 + dk, -hk / 2 - dk / 2, wk / 2, -hk / 2 - dk / 2, 180, 1)
 
     # Adding block labels
     bl_copper = ((wt - wcm) / 4 + wcm / 2, 0)
@@ -101,70 +101,70 @@ def current_flow_problem():
     bl_titanium = (wk / 2 + dk / 2, hk / 2 + dk / 2)
     bl_nomesh = (0, 0)
 
-    writer.lua_model.append(writer.add_blocklabel(bl_copper[0], bl_copper[1]))
-    writer.lua_model.append(writer.add_blocklabel(-bl_copper[0], bl_copper[1]))
+    writer.add_blocklabel(bl_copper[0], bl_copper[1])
+    writer.add_blocklabel(-bl_copper[0], bl_copper[1])
 
-    writer.lua_model.append(writer.add_blocklabel(bl_cmanganin[0], bl_cmanganin[1]))
-    writer.lua_model.append(writer.add_blocklabel(bl_cmanganin[0], -bl_cmanganin[1]))
+    writer.add_blocklabel(bl_cmanganin[0], bl_cmanganin[1])
+    writer.add_blocklabel(bl_cmanganin[0], -bl_cmanganin[1])
 
-    writer.lua_model.append(writer.add_blocklabel(bl_titanium[0], bl_titanium[1]))
-    writer.lua_model.append(writer.add_blocklabel(bl_titanium[0], -bl_titanium[1]))
-    writer.lua_model.append(writer.add_blocklabel(-bl_titanium[0], bl_titanium[1]))
-    writer.lua_model.append(writer.add_blocklabel(-bl_titanium[0], -bl_titanium[1]))
+    writer.add_blocklabel(bl_titanium[0], bl_titanium[1])
+    writer.add_blocklabel(bl_titanium[0], -bl_titanium[1])
+    writer.add_blocklabel(-bl_titanium[0], bl_titanium[1])
+    writer.add_blocklabel(-bl_titanium[0], -bl_titanium[1])
 
-    writer.lua_model.append(writer.add_blocklabel(bl_nomesh[0], bl_nomesh[1]))
+    writer.add_blocklabel(bl_nomesh[0], bl_nomesh[1])
 
     # Adding materials
     mat_copper = CurrentFlowMaterial("Copper", 58e6, 58e6, 0, 0, 0, 0)
     mat_cmanganin = CurrentFlowMaterial("Copper-Manganin", 20.833e6, 20.833e6, 0, 0, 0, 0)
     mat_titanium = CurrentFlowMaterial("Titanium", 1.789e6, 1.789e6, 0, 0, 0, 0)
 
-    writer.lua_model.append(writer.add_material(mat_copper))
-    writer.lua_model.append(writer.add_material(mat_cmanganin))
-    writer.lua_model.append(writer.add_material(mat_titanium))
+    writer.add_material(mat_copper)
+    writer.add_material(mat_cmanganin)
+    writer.add_material(mat_titanium)
 
-    writer.lua_model.append(writer.select_label(bl_copper[0], bl_copper[1]))
-    writer.lua_model.append(writer.select_label(-bl_copper[0], bl_copper[1]))
-    writer.lua_model.append(writer.set_blockprop("Copper"))
+    writer.select_label(bl_copper[0], bl_copper[1])
+    writer.select_label(-bl_copper[0], bl_copper[1])
+    writer.set_blockprop("Copper")
     writer.lua_model.append("ci_clearselected()")
 
-    writer.lua_model.append(writer.select_label(bl_cmanganin[0], bl_cmanganin[1]))
-    writer.lua_model.append(writer.select_label(bl_cmanganin[0], -bl_cmanganin[1]))
-    writer.lua_model.append(writer.set_blockprop("Copper-Manganin"))
+    writer.select_label(bl_cmanganin[0], bl_cmanganin[1])
+    writer.select_label(bl_cmanganin[0], -bl_cmanganin[1])
+    writer.set_blockprop("Copper-Manganin")
     writer.lua_model.append("ci_clearselected()")
 
-    writer.lua_model.append(writer.select_label(bl_titanium[0], bl_titanium[1]))
-    writer.lua_model.append(writer.select_label(bl_titanium[0], -bl_titanium[1]))
-    writer.lua_model.append(writer.select_label(-bl_titanium[0], bl_titanium[1]))
-    writer.lua_model.append(writer.select_label(-bl_titanium[0], -bl_titanium[1]))
-    writer.lua_model.append(writer.set_blockprop("Titanium"))
+    writer.select_label(bl_titanium[0], bl_titanium[1])
+    writer.select_label(bl_titanium[0], -bl_titanium[1])
+    writer.select_label(-bl_titanium[0], bl_titanium[1])
+    writer.select_label(-bl_titanium[0], -bl_titanium[1])
+    writer.set_blockprop("Titanium")
     writer.lua_model.append("ci_clearselected()")
 
-    writer.lua_model.append(writer.select_label(bl_nomesh[0], bl_nomesh[1]))
-    writer.lua_model.append(writer.set_blockprop("<No Mesh>"))
+    writer.select_label(bl_nomesh[0], bl_nomesh[1])
+    writer.set_blockprop("<No Mesh>")
     writer.lua_model.append("ci_clearselected()")
 
     # Adding boundary properties
     excitation = CurrentFlowSurfaceCurrent("Jin", J)
     ground = CurrentFlowFixedVoltage("GND", 0)
 
-    writer.lua_model.append(writer.add_boundary(excitation))
-    writer.lua_model.append(writer.add_boundary(ground))
+    writer.add_boundary(excitation)
+    writer.add_boundary(ground)
 
-    writer.lua_model.append(writer.select_segment(-wt / 2, 0))
-    writer.lua_model.append(writer.set_segment_prop("Jin"))
+    writer.select_segment(-wt / 2, 0)
+    writer.set_segment_prop("Jin")
     writer.lua_model.append("ci_clearselected()")
 
-    writer.lua_model.append(writer.select_segment(wt / 2, 0))
-    writer.lua_model.append(writer.set_segment_prop("GND"))
+    writer.select_segment(wt / 2, 0)
+    writer.set_segment_prop("GND")
     writer.lua_model.append("ci_clearselected()")
 
     writer.lua_model.append("ci_zoomnatural()")
     writer.lua_model.append("ci_zoomout()")
     writer.lua_model.append("hideconsole()")
-    writer.lua_model.append(writer.save_as("current_test.fec"))
-    writer.lua_model.append(writer.analyze())
-    writer.lua_model.append(writer.load_solution())
+    writer.save_as("current_test.fec")
+    writer.analyze()
+    writer.load_solution()
 
     # Examine the results
     writer.lua_model.append(f"co_selectblock({bl_copper[0]}, {bl_copper[1]})")
@@ -180,7 +180,7 @@ def current_flow_problem():
 
     writer.lua_model.append("P = co_blockintegral(0)")  # Power Loss
     writer.lua_model.append(writer.write_out_result("P", "P"))
-    writer.lua_model.extend(writer.close())
+    writer.close()
 
     # Execute the script
     writer.write("current_test.lua")
