af0f8f057f5d2f4a0396972f2d614f122d38e987
gadok <gado.krisztian@montana.hu>
2021-06-16 15:02:58 +0200

    fields added

--- adze_modeler/agros2d_wrapper.py
+++ adze_modeler/agros2d_wrapper.py
@@ -9,39 +9,197 @@ execute a script with gui:
 
 class Agros2DWrapper:
     def __init__(self):
-        self.problem_type = None
+        self.fields = {
+            "electrostatic": {},
+            "magnetic": {},
+            "current": {},
+            "heat": {},
+        }
         self.coordinate_type = "planar"
+        self.mesh_type = "triangle"
 
-    def set_problem_type(self, ptype):
+    def add_field_electrostatic(self, **kwargs):
         """
-        This function is used to set up the type of the problem:
-        Supported problems:
-            - Electrostatics
-            - Magnetics
-
-        :param ptype: Type of the problem. Possible values: 'electrostatic', 'magnetic'
+        :param analysis: 'steady'
+        :param solver: 'linear'
+        :param matrix_solver: 'umfpack', 'mumps', 'paraluation_it', 'paraluation_amg', 'mums_out'
+        :param nb_refinements: Greater than 0 integer.
+        :param nb_polyorder: Greater than 0 integer.
+        :param adaptivity: 'disabled', 'h', 'p', 'hp'
         """
 
-        if ptype.lower() in {"electrostatic", "magnetic"}:
-            self.problem_type = ptype.lower()
-        else:
-            raise NotImplementedError(f"This type of problem ('{ptype}') is not implemented. Typo maybe?")
+        analysis = kwargs.get("analysis", "steady")
+        solver = kwargs.get("solver", "linear")
+        matrix_solver = kwargs.get("matrix_solver", "umfpack")
+        nb_refinements = kwargs.get("nb_refinements", 1)
+        nb_polyorder = kwargs.get("nb_polyorder", 2)
+        adaptivity = kwargs.get("adaptivity", "disabled")
+
+        if analysis != "steady":
+            raise ValueError(f"Electric field can can be: 'steady'. Got '{analysis}' instead.")
+
+        if solver != "linear":
+            raise ValueError(f"Only 'linear' solver is supported.")
+
+        if matrix_solver not in {"umfpack", "mumps", "paraluation_it", "paraluation_amg", "mums_out"}:
+            raise ValueError(f'Imporper matrix_solver type: "{matrix_solver}"')
+
+        if not isinstance(nb_refinements, int) or nb_refinements < 1:
+            raise ValueError(f"Incorrect value for nb_refinements. ({nb_refinements})")
+
+        if not isinstance(nb_polyorder, int) or nb_polyorder < 1:
+            raise ValueError(f"Incorrect value for nb_polyorder. ({nb_polyorder})")
+
+        if adaptivity not in {"disabled", "h", "p", "hp"}:
+            raise ValueError(f"Incorrect value for adaptivity. ({adaptivity})")
+
+        self.fields["electrostatic"].update(kwargs)
+
+    def add_field_magnetic(self, **kwargs):
+        """
+        :param analysis: 'steady', 'transient', 'harmonic'
+        :param solver: 'linear', 'newton', 'picard'
+        :param matrix_solver: 'umfpack', 'mumps', 'paraluation_it', 'paraluation_amg', 'mums_out'
+        :param nb_refinements: Greater than 0 integer.
+        :param nb_polyorder: Greater than 0 integer.
+        :param adaptivity: 'disabled', 'h', 'p', 'hp'
+        """
+
+        analysis = kwargs.get("analysis", "steady")
+        solver = kwargs.get("solver", "linear")
+        matrix_solver = kwargs.get("matrix_solver", "umfpack")
+        nb_refinements = kwargs.get("nb_refinements", 1)
+        nb_polyorder = kwargs.get("nb_polyorder", 2)
+        adaptivity = kwargs.get("adaptivity", "disabled")
+
+        if analysis not in {"steady", "transient", "harmonic"}:
+            raise ValueError(f"Magnetic field can can be: 'steady', 'transient', 'harmonic'. Got '{analysis}' instead.")
+
+        if solver not in {"linear", "newton", "picard"}:
+            raise ValueError(f"Solver type can be: 'linear', 'newton', 'picard'. Got {solver}")
+
+        if matrix_solver not in {"umfpack", "mumps", "paraluation_it", "paraluation_amg", "mums_out"}:
+            raise ValueError(f'Imporper matrix_solver type: "{matrix_solver}"')
+
+        if not isinstance(nb_refinements, int) or nb_refinements < 1:
+            raise ValueError(f"Incorrect value for nb_refinements. ({nb_refinements})")
+
+        if not isinstance(nb_polyorder, int) or nb_polyorder < 1:
+            raise ValueError(f"Incorrect value for nb_polyorder. ({nb_polyorder})")
+
+        if adaptivity not in {"disabled", "h", "p", "hp"}:
+            raise ValueError(f"Incorrect value for adaptivity. ({adaptivity})")
+
+        self.fields["magnetic"].update(kwargs)
+
+    def add_field_heat_transfer(self, **kwargs):
+        """
+        :param analysis: 'steady', 'transient'
+        :param solver: 'linear', 'newton', 'picard'
+        :param matrix_solver: 'umfpack', 'mumps', 'paraluation_it', 'paraluation_amg', 'mums_out'
+        :param nb_refinements: Greater than 0 integer.
+        :param nb_polyorder: Greater than 0 integer.
+        :param adaptivity: 'disabled', 'h', 'p', 'hp'
+        """
+
+        analysis = kwargs.get("analysis", "steady")
+        solver = kwargs.get("solver", "linear")
+        matrix_solver = kwargs.get("matrix_solver", "umfpack")
+        nb_refinements = kwargs.get("nb_refinements", 1)
+        nb_polyorder = kwargs.get("nb_polyorder", 2)
+        adaptivity = kwargs.get("adaptivity", "disabled")
+
+        if analysis not in {"steady", "transient"}:
+            raise ValueError(f"Heat transfer field can can be: 'steady', 'transient'. Got '{analysis}' instead.")
+
+        if solver not in {"linear", "newton", "picard"}:
+            raise ValueError(f"Solver type can be: 'linear', 'newton', 'picard'. Got {solver}")
+
+        if matrix_solver not in {"umfpack", "mumps", "paraluation_it", "paraluation_amg", "mums_out"}:
+            raise ValueError(f'Imporper matrix_solver type: "{matrix_solver}"')
+
+        if not isinstance(nb_refinements, int) or nb_refinements < 1:
+            raise ValueError(f"Incorrect value for nb_refinements. ({nb_refinements})")
+
+        if not isinstance(nb_polyorder, int) or nb_polyorder < 1:
+            raise ValueError(f"Incorrect value for nb_polyorder. ({nb_polyorder})")
+
+        if adaptivity not in {"disabled", "h", "p", "hp"}:
+            raise ValueError(f"Incorrect value for adaptivity. ({adaptivity})")
+
+        self.fields["heat"].update(kwargs)
+
+    def add_field_current_flow(self, **kwargs):
+        """
+        :param analysis: 'steady'
+        :param solver: 'linear', 'newton'
+        :param matrix_solver: 'umfpack', 'mumps', 'paraluation_it', 'paraluation_amg', 'mums_out'
+        :param nb_refinements: Greater than 0 integer.
+        :param nb_polyorder: Greater than 0 integer.
+        :param adaptivity: 'disabled', 'h', 'p', 'hp'
+        """
+
+        analysis = kwargs.get("analysis", "steady")
+        solver = kwargs.get("solver", "linear")
+        matrix_solver = kwargs.get("matrix_solver", "umfpack")
+        nb_refinements = kwargs.get("nb_refinements", 1)
+        nb_polyorder = kwargs.get("nb_polyorder", 2)
+        adaptivity = kwargs.get("adaptivity", "disabled")
+
+        if analysis != "steady":
+            raise ValueError(f"Current flow field can can be: 'steady'. Got '{analysis}' instead.")
+
+        if solver not in {"linear", "newton"}:
+            raise ValueError(f"Solver type can be: 'linear', 'newton'. Got {solver}")
+
+        if matrix_solver not in {"umfpack", "mumps", "paraluation_it", "paraluation_amg", "mums_out"}:
+            raise ValueError(f'Imporper matrix_solver type: "{matrix_solver}"')
+
+        if not isinstance(nb_refinements, int) or nb_refinements < 1:
+            raise ValueError(f"Incorrect value for nb_refinements. ({nb_refinements})")
+
+        if not isinstance(nb_polyorder, int) or nb_polyorder < 1:
+            raise ValueError(f"Incorrect value for nb_polyorder. ({nb_polyorder})")
+
+        if adaptivity not in {"disabled", "h", "p", "hp"}:
+            raise ValueError(f"Incorrect value for adaptivity. ({adaptivity})")
+
+        self.fields["current"].update(kwargs)
 
     def set_coordinate_type(self, coordinate_type):
-        """ """
+        """
+        This functions sets the type of the coordinate being used.
+
+        :param coordinate_type: 'planar' or 'axisymmetric'
+        """
+
         if coordinate_type.lower() in {"planar", "axisymmetric"}:
             self.coordinate_type = coordinate_type.lower()
         else:
-            raise NotImplementedError(
-                f"This type of coordinate type ('{coordinate_type}') is not implemented. " f"Typo maybe?"
-            )
+            raise ValueError(f'There is no "{coordinate_type}" type of coordinate')
 
+    def set_mesh_type(self, mesh_type):
+        """
+        This functions sets the type of the mesh being used.
 
-class Agros2DExecutor:
-    def __init__(self):
-        pass
+        :param mesh_type: 'triangle', 'triangle_quad_fine_division', 'triangle_quad_rough_division',
+        'triangle_quad_join', 'gmsh_triangle', 'gmsh_quad', 'gmsh_quad_delaunay'
+        """
+
+        if mesh_type.lower() in {
+            "triangle",
+            "triangle_quad_fine_division",
+            "triangle_quad_rough_division",
+            "triangle_quad_join",
+            "gmsh_triangle",
+            "gmsh_quad",
+            "gmsh_quad_delaunay",
+        }:
+            self.mesh_type = mesh_type.lower()
+        else:
+            raise ValueError(f'There is no "{mesh_type}" type of mesh.')
 
 
 if __name__ == "__main__":
     ag = Agros2DWrapper()
-    ag.set_problem_type("alma")
+    ag.add_field_electrostatic(analysis="steady")
--- tests/test_agros2d_wrapper.py
+++ tests/test_agros2d_wrapper.py
@@ -4,19 +4,96 @@ from unittest import TestCase
 from adze_modeler.agros2d_wrapper import Agros2DWrapper
 
 
-class FemmTester(TestCase):
-    def get_fresh_wrapper(self):
-        return Agros2DWrapper()
+class Agros2DTester(TestCase):
 
-    def test_set_problem_type(self):
-        # TODO: test for other types of problems
-        w = self.get_fresh_wrapper()
+    def test_add_field_electrostatic(self):
+        w = Agros2DWrapper()
 
-        w.set_problem_type("electrostatic")
-        self.assertEqual(w.problem_type, "electrostatic")
+        self.assertRaises(ValueError, w.add_field_electrostatic, analysis="eper")
+        self.assertRaises(ValueError, w.add_field_electrostatic, solver="eper")
+        self.assertRaises(ValueError, w.add_field_electrostatic, matrix_solver="eper")
+        self.assertRaises(ValueError, w.add_field_electrostatic, nb_refinements=0.2)
+        self.assertRaises(ValueError, w.add_field_electrostatic, nb_polyorder=-4)
+        self.assertRaises(ValueError, w.add_field_electrostatic, adaptivity="eper")
 
-        w.set_problem_type("magnetic")
-        self.assertEqual(w.problem_type, "magnetic")
+        w.add_field_electrostatic(nb_polyorder=3)
+        self.assertEqual(w.fields['electrostatic']['nb_polyorder'], 3)
 
-        # test for nonexisting field
-        self.assertRaises(NotImplementedError, w.set_problem_type, "eper")
+    def test_add_field_magnetic(self):
+        w = Agros2DWrapper()
+
+        self.assertRaises(ValueError, w.add_field_magnetic, analysis="eper")
+        self.assertRaises(ValueError, w.add_field_magnetic, solver="eper")
+        self.assertRaises(ValueError, w.add_field_magnetic, matrix_solver="eper")
+        self.assertRaises(ValueError, w.add_field_magnetic, nb_refinements=0.2)
+        self.assertRaises(ValueError, w.add_field_magnetic, nb_polyorder=-4)
+        self.assertRaises(ValueError, w.add_field_magnetic, adaptivity="eper")
+
+        w.add_field_magnetic(analysis='transient')
+        self.assertEqual(w.fields['magnetic']['analysis'], 'transient')
+
+    def test_add_field_heat_transfer(self):
+        w = Agros2DWrapper()
+
+        self.assertRaises(ValueError, w.add_field_heat_transfer, analysis="eper")
+        self.assertRaises(ValueError, w.add_field_heat_transfer, solver="eper")
+        self.assertRaises(ValueError, w.add_field_heat_transfer, matrix_solver="eper")
+        self.assertRaises(ValueError, w.add_field_heat_transfer, nb_refinements=0.2)
+        self.assertRaises(ValueError, w.add_field_heat_transfer, nb_polyorder=-4)
+        self.assertRaises(ValueError, w.add_field_heat_transfer, adaptivity="eper")
+
+        w.add_field_heat_transfer(analysis='transient')
+        self.assertEqual(w.fields['heat']['analysis'], 'transient')
+
+    def test_add_field_current_flow(self):
+        w = Agros2DWrapper()
+
+        self.assertRaises(ValueError, w.add_field_current_flow, analysis="eper")
+        self.assertRaises(ValueError, w.add_field_current_flow, solver="eper")
+        self.assertRaises(ValueError, w.add_field_current_flow, matrix_solver="eper")
+        self.assertRaises(ValueError, w.add_field_current_flow, nb_refinements=0.2)
+        self.assertRaises(ValueError, w.add_field_current_flow, nb_polyorder=-4)
+        self.assertRaises(ValueError, w.add_field_current_flow, adaptivity="eper")
+
+        w.add_field_current_flow(nb_refinement=3)
+        self.assertEqual(w.fields['current']['nb_refinement'], 3)
+
+
+    def test_set_coordinate_type(self):
+        w = Agros2DWrapper()
+
+        w.set_coordinate_type('PLANAR')
+        self.assertEqual(w.coordinate_type, "planar")
+
+        w.set_coordinate_type('Axisymmetric')
+        self.assertEqual(w.coordinate_type, "axisymmetric")
+
+        self.assertRaises(ValueError, w.set_coordinate_type, "eper")
+
+
+    def test_set_mesh_type(self):
+        w = Agros2DWrapper()
+
+        w.set_mesh_type("triangle")
+        self.assertEqual(w.mesh_type, "triangle")
+
+        w.set_mesh_type("triangle_quad_fine_division")
+        self.assertEqual(w.mesh_type, "triangle_quad_fine_division")
+
+        w.set_mesh_type("triangle_quad_rough_division")
+        self.assertEqual(w.mesh_type, "triangle_quad_rough_division")
+
+        w.set_mesh_type("triangle_quad_join")
+        self.assertEqual(w.mesh_type, "triangle_quad_join")
+
+        w.set_mesh_type("gmsh_triangle")
+        self.assertEqual(w.mesh_type, "gmsh_triangle")
+
+        w.set_mesh_type("gmsh_quad")
+        self.assertEqual(w.mesh_type, "gmsh_quad")
+
+        w.set_mesh_type("gmsh_quad_delaunay")
+        self.assertEqual(w.mesh_type, "gmsh_quad_delaunay")
+
+
+        self.assertRaises(ValueError, w.set_mesh_type, "eper")
