fa050a0f7c06c9ad40544ee6c81211b508ac044d
Tamas Orosz <oroszt@montana.hu>
2021-06-19 13:46:24 +0200

    test for single step castejeu construction

--- adze_modeler/geometry.py
+++ adze_modeler/geometry.py
@@ -45,12 +45,6 @@ class Geometry:
         self.nodes.append(cb.control2)
         self.nodes.append(cb.end_pt)
 
-    def bezier2polygon(self, tolerance=1e-2):
-
-
-
-        return
-
     def delete_hanging_nodes(self):
         """Delete all nodes, which not part of a another object (Line, Circle, etc)"""
         temp = []
@@ -167,6 +161,54 @@ class Geometry:
 
         return
 
+    @staticmethod
+    def casteljau(bezier: obj.CubicBezier):
+        """
+        Gets a Bezier object and makes only one Casteljau's iteration step on it without the recursion.
+
+        The algorithm splits the bezier into two, smaller parts denoted by r is the 'right-sides' and l denotes the
+        'left sided' one. The algorithm is limited to use cubic beziers only.
+
+        :return: 2 bezier objects, the right and the left one
+
+        """
+        # calculating the mid point [m]
+        m = (bezier.control1 + bezier.control2) * 0.5
+
+        l0 = bezier.start_pt
+        r3 = bezier.end_pt
+
+        l1 = (bezier.start_pt + bezier.control1) * 0.5
+        r2 = (bezier.control2 + bezier.end_pt) * 0.5
+
+        l2 = (l1 + m) * 0.5
+        r1 = (r2 + m) * 0.5
+
+        l3 = (l2 + r1) * 0.5
+        r0 = l3
+
+        r = obj.CubicBezier(start_pt=r0, control1=r1, control2=r2, end_pt=r3)
+        l = obj.CubicBezier(start_pt=l0, control1=l1, control2=l2, end_pt=l3)
+
+        return r, l
+
+    def export_svg(self, file_name):
+        """
+        Creates an svg image from the geometry objects.
+        """
+
+        # every object handled as a separate path
+
+        obj.Line
+        paths = []
+        # exports the lines
+        for seg in self.lines:
+            path = svg.Path()
+            path.append(svg.Line(complex(seg.start_pt.x, seg.start_pt.y), complex(seg.end_pt.x, seg.end_pt.y)))
+            paths.append(path)
+
+        svg.wsvg(paths, svgwrite_debug=True, filename='output1.svg')
+
     def import_svg(self, svg_img, *args):
         """Imports the svg file into a new geo object. The function gives an automatic id to the function
 
@@ -266,10 +308,10 @@ class Geometry:
 
         else:
             up = (-x1 * y2 + x1 * y3 + x2 * y1 - x2 * y3 - x3 * y1 + x3 * y2) / (
-                x1 * y3 - x1 * y4 - x2 * y3 + x2 * y4 - x3 * y1 + x3 * y2 + x4 * y1 - x4 * y2
+                    x1 * y3 - x1 * y4 - x2 * y3 + x2 * y4 - x3 * y1 + x3 * y2 + x4 * y1 - x4 * y2
             )
             tp = (x1 * y3 - x1 * y4 - x3 * y1 + x3 * y4 + x4 * y1 - x4 * y3) / (
-                x1 * y3 - x1 * y4 - x2 * y3 + x2 * y4 - x3 * y1 + x3 * y2 + x4 * y1 - x4 * y2
+                    x1 * y3 - x1 * y4 - x2 * y3 + x2 * y4 - x3 * y1 + x3 * y2 + x4 * y1 - x4 * y2
             )
             if inrange(tp) and inrange(up):
                 p1 = tuple(p + tp * r)
@@ -319,7 +361,6 @@ class Geometry:
 
     def getid(self):
         return int(uuid4())
-
 
 # def node_gmsh_point_distance(node, point):
 #     dx = node.x - point.x[0]
---
+++ tests/test_bezier_approximation.py
@@ -0,0 +1,47 @@
+import unittest
+from adze_modeler.geometry import Geometry
+from adze_modeler.objects import CubicBezier, Node, Line
+from importlib_resources import files
+import uuid
+
+
+class TestBezierApprox(unittest.TestCase):
+
+    def test_casteljeu_step(self):
+        # Test Nodes
+        a = Node(x=0.0, y=0.0, id=1)
+        b = Node(x=1.0, y=-0.2, id=2)
+        c1 = Node(x=0.2, y=0.9, id=3)
+        c2 = Node(x=0.8, y=0.7, id=4)
+
+        bez = CubicBezier(a, c1, c2, b)
+
+        r, l = Geometry.casteljau(bez)
+
+        self.assertEqual(r.end_pt.x, 1.0)
+        self.assertEqual(r.end_pt.y, -0.2)
+
+        self.assertEqual(l.control1.x, 0.1)
+        self.assertEqual(l.control1.y, 0.45)
+
+    # def test_1(self):
+    #     #eml = files("tests.svgtests").joinpath("test_bez_approx.svg")
+    #     eml = files("examples.owl").joinpath("owl-shape.svg")
+    #
+    #     geo = Geometry()
+    #     geo.import_svg(eml.as_posix())
+    #
+    #     for bezier in geo.cubic_beziers:
+    #         r, l = geo.casteljau(bezier)
+    #         geo.add_line(Line(r.start_pt, r.control1))
+    #         geo.add_line(Line(r.control1, r.control2))
+    #         geo.add_line(Line(r.control2, r.end_pt))
+    #
+    #         geo.add_line(Line(l.start_pt, l.control1))
+    #         geo.add_line(Line(l.control1, l.control2))
+    #         geo.add_line(Line(l.control2, l.end_pt))
+    #
+    #     print(geo.lines)
+    #     print(len(geo.lines))
+    #
+    #     geo.export_svg('test.svg')
