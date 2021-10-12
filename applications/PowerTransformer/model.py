e62e97418eef21067feabb32d9dad5cfdaf50f19
gadokrisztian <gado.krisztian@montana.hu>
2021-10-12 08:12:44 +0200

    transformer model init

--- applications/PowerTransformer/model.py
+++ applications/PowerTransformer/model.py
@@ -8,22 +8,21 @@ from adze_modeler.modelpaths import ModelDir
 
 ModelDir.set_base(__file__)
 
-class ${name}(BaseModel):
-    """docstring for ${name}"""
+class PowerTransformer(BaseModel):
+    """docstring for PowerTransformer"""
     def __init__(self, **kwargs):
         exportname = kwargs.get('exportname', None)
-        super(${name}, self).__init__()
+        super(PowerTransformer, self).__init__()
         self._init_directories()
 
     def setup_solver(self):
         femm_metadata = FemmMetadata()
         femm_metadata.problem_type = "magnetic"
-        femm_metadata.coordinate_type = "planar"
+        femm_metadata.coordinate_type = "axisymmetric"
         femm_metadata.file_script_name = self.file_solver_script
         femm_metadata.file_metrics_name = self.file_solution
         femm_metadata.unit = "millimeters"
         femm_metadata.smartmesh = True
-        femm_metadata.depth = 1000
 
         self.platform = Femm(femm_metadata)
         self.snapshot = Snapshot(self.platform)
@@ -40,8 +39,8 @@ class ${name}(BaseModel):
         self.snapshot.add_boundary_condition(a0)
 
     def add_postprocessing(self):
-        points = [(0, 0)]
-        self.snapshot.add_postprocessing("integration", points, "Energy")
+        points = [(0, 0), (0, 0)]
+        self.snapshot.add_postprocessing("integration", points, "Torque")
         
     def build_geometry(self):
         # ...
@@ -49,5 +48,5 @@ class ${name}(BaseModel):
         
 
 if __name__ == "__main__":
-    m = ${name}(exportname="dev")
-    print(m(cleanup=False))
+    m = PowerTransformer(exportname="dev")
+    print(m(cleanup=False, devmode=True))
