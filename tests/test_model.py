8d972aa666e330190b08804dac243496a3f8e7ec
gadokrisztian <gadokrisztian@corp.laptop>
2021-10-28 12:58:58 +0200

    model tests fixed

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,8 +9,8 @@ from adze_modeler.snapshot import Snapshot
 
 
 class MockModel(BaseModel):
-    def __init__(self, exportname: str = None):
-        super().__init__(exportname)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def setup_solver(self):
         super().setup_solver()
@@ -52,28 +52,28 @@ class TestModel(unittest.TestCase):
         # self.dir_export = self.dir_snapshots / self.name
         self.dir_export = self.dir_snapshots
 
-    # def test_model_paths(self):
-    #     m = MockModel(exportname="test_name")
-    #     self.assertEqual(self.dir_current, m.dir_current)
-    #     self.assertEqual(self.dir_resources, m.dir_resources)
-    #     self.assertEqual(self.dir_snapshots, m.dir_snapshots)
-    #     self.assertEqual(self.dir_media, m.dir_media)
-    #     self.assertEqual(self.dir_data, m.dir_data)
-    #     self.assertEqual(self.dir_export, m.dir_export.parent)
-    #
-    #     # create paths
-    #     m._init_directories()
-    #     self.assertTrue(self.dir_current.exists())
-    #     self.assertTrue(self.dir_resources.exists())
-    #     self.assertTrue(self.dir_snapshots.exists())
-    #     self.assertTrue(self.dir_media.exists())
-    #     self.assertTrue(self.dir_data.exists())
-    #     self.assertTrue(self.dir_export.exists())
-    #
-    #     m(cleanup=True, timeout=0)
-    #     rmtree(m.dir_snapshots)
-    #
-    #     # teardown
-    #     self.dir_resources.rmdir()
-    #     self.dir_media.rmdir()
-    #     self.dir_data.rmdir()
+    def test_model_paths(self):
+        m = MockModel(exportname="test_name")
+        self.assertEqual(self.dir_current, m.dir_current)
+        self.assertEqual(self.dir_resources, m.dir_resources)
+        self.assertEqual(self.dir_snapshots, m.dir_snapshots)
+        self.assertEqual(self.dir_media, m.dir_media)
+        self.assertEqual(self.dir_data, m.dir_data)
+        self.assertEqual(self.dir_export, m.dir_export.parent)
+
+        # create paths
+        m._init_directories()
+        self.assertTrue(self.dir_current.exists())
+        self.assertTrue(self.dir_resources.exists())
+        self.assertTrue(self.dir_snapshots.exists())
+        self.assertTrue(self.dir_media.exists())
+        self.assertTrue(self.dir_data.exists())
+        self.assertTrue(self.dir_export.exists())
+
+        rmtree(m.dir_snapshots)
+
+        # teardown
+        self.dir_resources.rmdir()
+        self.dir_media.rmdir()
+        self.dir_data.rmdir()
+
