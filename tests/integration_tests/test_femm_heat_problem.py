5edcd994f4e28309a39045feb3571df6a705984b
gadokrisztian <gadokrisztian@corp.laptop>
2021-10-28 12:58:09 +0200

    integration tests fixed

--- tests/integration_tests/test_femm_heat_problem.py
+++ tests/integration_tests/test_femm_heat_problem.py
@@ -12,103 +12,114 @@ from importlib_resources import files
 def c2k(C):
     return C + 274.15
 
-#
-# class TestFemmCurrentFlowProblem(unittest.TestCase):
-#     def test_heat_problem(self):
-#         writer = FemmWriter()
-#         writer.field = femm_heat_flow
-#         writer.init_problem("heat_data.csv")
-#
-#         writer.heat_problem("meters", "planar")
-#
-#         w1 = 0.3 + 0.2 + 0.3
-#         h1 = 0.4
-#         w2 = 0.2
-#         h2 = 0.2
-#
-#         # Constructing the geometry
-#         writer.add_node(-w1 / 2, -h1 / 2)
-#         writer.add_node(w1 / 2, -h1 / 2)
-#         writer.add_node(w1 / 2, h1 / 2)
-#         writer.add_node(-w1 / 2, h1 / 2)
-#
-#         writer.add_node(-w2 / 2, -h1 / 2)
-#         writer.add_node(-w2 / 2, -h1 / 2 + h2)
-#         writer.add_node(w2 / 2, -h1 / 2 + h2)
-#         writer.add_node(w2 / 2, -h1 / 2)
-#
-#         # adding segments
-#         writer.add_segment(-w1 / 2, -h1 / 2, -w2 / 2, -h1 / 2)
-#         writer.add_segment(-w2 / 2, -h1 / 2, -w2 / 2, -h1 / 2 + h2)
-#         writer.add_segment(-w2 / 2, -h1 / 2 + h2, w2 / 2, -h1 / 2 + h2)
-#         writer.add_segment(w2 / 2, -h1 / 2 + h2, w2 / 2, -h1 / 2)
-#         writer.add_segment(w2 / 2, -h1 / 2, w1 / 2, -h1 / 2)
-#         writer.add_segment(w1 / 2, -h1 / 2, w1 / 2, h1 / 2)
-#         writer.add_segment(w1 / 2, h1 / 2, -w1 / 2, h1 / 2)
-#         writer.add_segment(-w1 / 2, h1 / 2, -w1 / 2, -h1 / 2)
-#
-#         # Adding material properties
-#         blocklabel = (0.0, h1 / 2.0 * 0.6)
-#         mat = HeatFlowMaterial("Material", 1380, 1380, 0, 0)
-#         writer.add_material(mat)
-#         writer.add_blocklabel(*blocklabel)
-#         writer.select_label(*blocklabel)
-#         writer.set_blockprop("Material")
-#
-#         # Adding boundary properties
-#         insulation = HeatFlowConvection("ins", 0, c2k(22))
-#         convection = HeatFlowConvection("conv", 50, c2k(50))
-#         fixtemp = HeatFlowFixedTemperature("fixtemp", c2k(300))
-#         writer.add_boundary(insulation)
-#         writer.add_boundary(convection)
-#         writer.add_boundary(fixtemp)
-#
-#         # Set boundary properties
-#
-#         # insulation
-#         writer.select_segment(-w1 / 2, 0)
-#         writer.select_segment(w1 / 2, 0)
-#         writer.set_segment_prop("ins")
-#         writer.lua_model.append("hi_clearselected()")
-#
-#         # convection
-#         writer.select_segment(0, h1 / 2)
-#         writer.set_segment_prop("conv")
-#         writer.lua_model.append("hi_clearselected()")
-#
-#         # fixed temperature
-#         writer.select_segment(-(w2 - w1) / 4, -h1 / 2)
-#         writer.select_segment((w2 - w1) / 4, -h1 / 2)
-#         writer.select_segment(-w2 / 2, -h1 / 2 + h2 / 2)
-#         writer.select_segment(w2 / 2, -h1 / 2 + h2 / 2)
-#         writer.select_segment(0, -h1 / 2 + h2)
-#         writer.set_segment_prop("fixtemp")
-#         writer.lua_model.append("hi_clearselected()")
-#
-#         writer.lua_model.append("hi_zoomnatural()")
-#         writer.lua_model.append("hi_zoomout()")
-#         writer.lua_model.append("hideconsole()")
-#         writer.save_as("heatflow_test.feh")
-#         writer.analyze()
-#         writer.load_solution()
-#
-#         # select the material
-#         writer.lua_model.append("ho_selectblock(0, 0.1)")
-#         writer.lua_model.append("Fx, Fy = ho_blockintegral(3)")
-#         writer.write_out_result("Fx", "Fx")
-#         writer.write_out_result("Fy", "Fy")
-#         writer.close()
-#
-#         try:
-#             reference = files("tests.integration_tests").joinpath("heatflow_test.lua")
-#             with open(reference) as f:
-#                 content = f.readlines()
-#                 counter_test = Counter(content)
-#                 counter_reference = Counter(writer.lua_model)
-#
-#                 for key in counter_reference.keys():
-#                     # print(f'|{key}|', counter_reference[key.rstrip()], counter_test[key + "\n"])
-#                     self.assertEqual(counter_reference[key.rstrip()], counter_test[key + "\n"])
-#
-#         except FileNotFoundError:
-#             self.assertTrue(False)
+
+class TestFemmCurrentFlowProblem(unittest.TestCase):
+    def test_heat_problem(self):
+        writer = FemmWriter()
+        writer.field = femm_heat_flow
+        writer.init_problem("heat_data.csv")
+
+        writer.heat_problem("meters", "planar")
+
+        w1 = 0.3 + 0.2 + 0.3
+        h1 = 0.4
+        w2 = 0.2
+        h2 = 0.2
+
+        # Constructing the geometry
+        writer.add_node(-w1 / 2, -h1 / 2)
+        writer.add_node(w1 / 2, -h1 / 2)
+        writer.add_node(w1 / 2, h1 / 2)
+        writer.add_node(-w1 / 2, h1 / 2)
+
+        writer.add_node(-w2 / 2, -h1 / 2)
+        writer.add_node(-w2 / 2, -h1 / 2 + h2)
+        writer.add_node(w2 / 2, -h1 / 2 + h2)
+        writer.add_node(w2 / 2, -h1 / 2)
+
+        # adding segments
+        writer.add_segment(-w1 / 2, -h1 / 2, -w2 / 2, -h1 / 2)
+        writer.add_segment(-w2 / 2, -h1 / 2, -w2 / 2, -h1 / 2 + h2)
+        writer.add_segment(-w2 / 2, -h1 / 2 + h2, w2 / 2, -h1 / 2 + h2)
+        writer.add_segment(w2 / 2, -h1 / 2 + h2, w2 / 2, -h1 / 2)
+        writer.add_segment(w2 / 2, -h1 / 2, w1 / 2, -h1 / 2)
+        writer.add_segment(w1 / 2, -h1 / 2, w1 / 2, h1 / 2)
+        writer.add_segment(w1 / 2, h1 / 2, -w1 / 2, h1 / 2)
+        writer.add_segment(-w1 / 2, h1 / 2, -w1 / 2, -h1 / 2)
+
+        # Adding material properties
+        blocklabel = (0.0, h1 / 2.0 * 0.6)
+        mat = HeatFlowMaterial("Material", 1380, 1380, 0, 0)
+        writer.add_material(mat)
+        writer.add_blocklabel(*blocklabel)
+        writer.select_label(*blocklabel)
+        writer.set_blockprop("Material")
+
+        # Adding boundary properties
+        insulation = HeatFlowConvection("ins", 0, c2k(22))
+        convection = HeatFlowConvection("conv", 50, c2k(50))
+        fixtemp = HeatFlowFixedTemperature("fixtemp", c2k(300))
+        writer.add_boundary(insulation)
+        writer.add_boundary(convection)
+        writer.add_boundary(fixtemp)
+
+        # Set boundary properties
+
+        # insulation
+        writer.select_segment(-w1 / 2, 0)
+        writer.select_segment(w1 / 2, 0)
+        writer.set_segment_prop("ins")
+        writer.lua_model.append("hi_clearselected()")
+
+        # convection
+        writer.select_segment(0, h1 / 2)
+        writer.set_segment_prop("conv")
+        writer.lua_model.append("hi_clearselected()")
+
+        # fixed temperature
+        writer.select_segment(-(w2 - w1) / 4, -h1 / 2)
+        writer.select_segment((w2 - w1) / 4, -h1 / 2)
+        writer.select_segment(-w2 / 2, -h1 / 2 + h2 / 2)
+        writer.select_segment(w2 / 2, -h1 / 2 + h2 / 2)
+        writer.select_segment(0, -h1 / 2 + h2)
+        writer.set_segment_prop("fixtemp")
+        writer.lua_model.append("hi_clearselected()")
+
+        writer.lua_model.append("hi_zoomnatural()")
+        writer.lua_model.append("hi_zoomout()")
+        writer.lua_model.append("hideconsole()")
+        writer.save_as("heatflow_test.feh")
+        writer.analyze()
+        writer.load_solution()
+
+        # select the material
+        writer.lua_model.append("ho_selectblock(0, 0.1)")
+        writer.lua_model.append("Fx, Fy = ho_blockintegral(3)")
+        writer.write_out_result("Fx", "Fx")
+        writer.write_out_result("Fy", "Fy")
+        writer.close()
+
+        try:
+            reference = files("tests.integration_tests").joinpath("heatflow_test.lua")
+            with open(reference) as f:
+                content = f.readlines()
+                counter_test = Counter(content)
+                counter_reference = Counter(writer.lua_model)
+
+                for key in counter_reference.keys():
+                    # print(f'|{key}|', counter_reference[key.rstrip()], counter_test[key + "\n"])
+
+                    # filter out path related commands
+                    if "remove(" in key:
+                        continue
+
+                    if "saveas" in key:
+                        continue
+
+                    if "openfile" in key:
+                        continue
+
+                    self.assertEqual(counter_reference[key.rstrip()], counter_test[key + "\n"])
+
+        except FileNotFoundError:
+            self.assertTrue(False)
--- tests/integration_tests/test_femm_magnetic_problem.py
+++ tests/integration_tests/test_femm_magnetic_problem.py
@@ -8,106 +8,116 @@ from adze_modeler.femm_wrapper import MagneticMixed
 from importlib_resources import files
 
 
-# class TestFemmWriterWithExecutor(unittest.TestCase):
-#     """Tries to run a simple FEMM model, which is made with the FemmWriter class"""
-#
-#     def test_air_core_coil_inductance_lua(self):
-#         """
-#         mi_loadsolution;
-#         c = mo_getcircuitproperties('icoil');
-#         y = c(3);
-#         closefemm;
-#         """
-#         # n = 100, ri = 1, ro = 2, z = 1
-#         # FEMM inductance =  0.64707 mH
-#
-#         writer = FemmWriter()
-#         writer.lua_model = []  #
-#         writer.init_problem(out_file="femm_data.csv")
-#
-#         # problem definition
-#         writer.magnetic_problem(0, "inches", "axi")
-#
-#         # model geometry
-#         # rectangle (coil) -- ri, -z / 2, ro, z / 2 --
-#         n = 100
-#         ri = 1.0
-#         ro = 2.0
-#         z = 1.0
-#         r = 2.0 * max(ri, ro, z)
-#
-#         # nodes
-#         z = 0.5 * z
-#         writer.add_node(ri, -z)
-#         writer.add_node(ri, z)
-#         writer.add_node(ro, -z)
-#         writer.add_node(ro, z)
-#
-#         writer.add_segment(ri, -z, ri, z)
-#         writer.add_segment(ri, z, ro, z)
-#         writer.add_segment(ro, z, ro, -z)
-#         writer.add_segment(ro, -z, ri, -z)
-#
-#         # air region
-#         writer.add_node(0, -r)
-#         writer.add_node(0, r)
-#         writer.add_segment(0, -r, 0, r)
-#
-#         writer.add_arc(0.0, -r, 0.0, r, 180, 5)
-#
-#         # circle properties and material definitions
-#         writer.add_circprop("icoil", 1, 1)
-#
-#         writer.add_blocklabel((ri + ro) / 2, 0)
-#         writer.add_blocklabel(0.75 * r, 0)
-#
-#         # add material properties
-#         coil = MagneticMaterial("coil", 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
-#         air = MagneticMaterial("air", 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
-#
-#         writer.add_material(coil)
-#         writer.add_material(air)
-#
-#         # add boundary properties
-#         mixed_boundary = MagneticMixed("abc", 1.0 / (r * 0.0254 * pi * 4e-7), 0)
-#         writer.add_boundary(mixed_boundary)
-#
-#         # set coil property
-#         writer.select_label((ri + ro) / 2, 0)
-#         writer.set_blockprop("coil", 0, r / 20, 0, circuit_name="icoil", turns=n, magdirection=0)
-#         writer.clear_selected()
-#
-#         # set air
-#         writer.select_label(0.75 * r, 0)
-#         writer.set_blockprop("air", 0, r / 100, 0, circuit_name="<None>", turns=0, magdirection=0)
-#         writer.clear_selected()
-#
-#         # set boundaries
-#         writer.select_arc_segment(r, 0)
-#         writer.set_arc_segment_prop(5, "abc", 0, 0)
-#
-#         # the model have to be saved into a femm format to be used from the FEMM-wrapper script
-#         writer.save_as("test.fem")
-#         writer.analyze()
-#         writer.load_solution()
-#         writer.get_circuit_properties("icoil", result="current, volt, flux")
-#         writer.write_out_result("current", "current")
-#         writer.write_out_result("volt", "volt")
-#         writer.write_out_result("flux", "flux")
-#
-#         writer.close()
-#         writer.write("magnetic_ref.lua")
-#
-#         try:
-#             reference = files("tests.integration_tests").joinpath("magnetic.lua")
-#             with open(reference) as f:
-#                 content = f.readlines()
-#                 counter_test = Counter(content)
-#                 counter_reference = Counter(writer.lua_model)
-#
-#                 for key in counter_reference.keys():
-#                     # print(f'|{key}|', counter_reference[key.rstrip()], counter_test[key + "\n"])
-#                     self.assertEqual(counter_reference[key.rstrip()], counter_test[key + "\n"])
-#
-#         except FileNotFoundError:
-#             self.assertTrue(False)
+class TestFemmWriterWithExecutor(unittest.TestCase):
+    """Tries to run a simple FEMM model, which is made with the FemmWriter class"""
+
+    def test_air_core_coil_inductance_lua(self):
+        """
+        mi_loadsolution;
+        c = mo_getcircuitproperties('icoil');
+        y = c(3);
+        closefemm;
+        """
+        # n = 100, ri = 1, ro = 2, z = 1
+        # FEMM inductance =  0.64707 mH
+
+        writer = FemmWriter()
+        writer.lua_model = []  #
+        writer.init_problem(out_file="femm_data.csv")
+
+        # problem definition
+        writer.magnetic_problem(0, "inches", "axi")
+
+        # model geometry
+        # rectangle (coil) -- ri, -z / 2, ro, z / 2 --
+        n = 100
+        ri = 1.0
+        ro = 2.0
+        z = 1.0
+        r = 2.0 * max(ri, ro, z)
+
+        # nodes
+        z = 0.5 * z
+        writer.add_node(ri, -z)
+        writer.add_node(ri, z)
+        writer.add_node(ro, -z)
+        writer.add_node(ro, z)
+
+        writer.add_segment(ri, -z, ri, z)
+        writer.add_segment(ri, z, ro, z)
+        writer.add_segment(ro, z, ro, -z)
+        writer.add_segment(ro, -z, ri, -z)
+
+        # air region
+        writer.add_node(0, -r)
+        writer.add_node(0, r)
+        writer.add_segment(0, -r, 0, r)
+
+        writer.add_arc(0.0, -r, 0.0, r, 180, 5)
+
+        # circle properties and material definitions
+        writer.add_circprop("icoil", 1, 1)
+
+        writer.add_blocklabel((ri + ro) / 2, 0)
+        writer.add_blocklabel(0.75 * r, 0)
+
+        # add material properties
+        coil = MagneticMaterial("coil", 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+        air = MagneticMaterial("air", 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+
+        writer.add_material(coil)
+        writer.add_material(air)
+
+        # add boundary properties
+        mixed_boundary = MagneticMixed("abc", 1.0 / (r * 0.0254 * pi * 4e-7), 0)
+        writer.add_boundary(mixed_boundary)
+
+        # set coil property
+        writer.select_label((ri + ro) / 2, 0)
+        writer.set_blockprop("coil", 0, r / 20, 0, circuit_name="icoil", turns=n, magdirection=0)
+        writer.clear_selected()
+
+        # set air
+        writer.select_label(0.75 * r, 0)
+        writer.set_blockprop("air", 0, r / 100, 0, circuit_name="<None>", turns=0, magdirection=0)
+        writer.clear_selected()
+
+        # set boundaries
+        writer.select_arc_segment(r, 0)
+        writer.set_arc_segment_prop(5, "abc", 0, 0)
+
+        # the model have to be saved into a femm format to be used from the FEMM-wrapper script
+        writer.save_as("test.fem")
+        writer.analyze()
+        writer.load_solution()
+        writer.get_circuit_properties("icoil", result="current, volt, flux")
+        writer.write_out_result("current", "current")
+        writer.write_out_result("volt", "volt")
+        writer.write_out_result("flux", "flux")
+
+        writer.close()
+        writer.write("magnetic_ref.lua")
+
+        try:
+            reference = files("tests.integration_tests").joinpath("magnetic.lua")
+            with open(reference) as f:
+                content = f.readlines()
+                counter_test = Counter(content)
+                counter_reference = Counter(writer.lua_model)
+
+                for key in counter_reference.keys():
+                    # print(f'|{key}|', counter_reference[key.rstrip()], counter_test[key + "\n"])
+                    # filter out path related commands
+                    if "remove(" in key:
+                        continue
+
+                    if "saveas" in key:
+                        continue
+
+                    if "openfile" in key:
+                        continue
+
+                    self.assertEqual(counter_reference[key.rstrip()], counter_test[key + "\n"])
+
+        except FileNotFoundError:
+            self.assertTrue(False)
