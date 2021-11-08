5576f6a2976988234dd5d223b58b0df94be71fca
gadokrisztian <gado.krisztian@montana.hu>
2021-11-08 12:09:03 +0100

    test for ModelDir added

---
+++ tests/test_modeldirs.py
@@ -0,0 +1,32 @@
+import unittest
+from adze_modeler.modelpaths import ModelDir
+from pathlib import Path
+
+CURRENT = Path(__file__)
+
+class TestModeldirs(unittest.TestCase):
+
+    def test_modeldir(self):
+        ModelDir.set_base(CURRENT)
+
+        self.assertEqual(ModelDir.BASE, CURRENT.parent)
+        self.assertEqual(ModelDir.MEDIA, CURRENT.parent / "media")
+        self.assertEqual(ModelDir.DATA, CURRENT.parent / "data")
+        self.assertEqual(ModelDir.RESOURCES, CURRENT.parent / "resources")
+        self.assertEqual(ModelDir.SNAPSHOTS, CURRENT.parent / "snapshots")
+        self.assertEqual(ModelDir.DEFAULTS, CURRENT.parent / "defaults")
+        self.assertEqual(ModelDir.DOCS, CURRENT.parent / "docs")
+
+
+    def test_dir_iter(self):
+        ModelDir.set_base(CURRENT.parent)
+
+        dirs = ModelDir.get_dirs()
+
+        self.assertEqual(next(dirs), CURRENT.parent)
+        self.assertEqual(next(dirs), CURRENT.parent / "media")
+        self.assertEqual(next(dirs), CURRENT.parent / "data")
+        self.assertEqual(next(dirs), CURRENT.parent / "resources")
+        self.assertEqual(next(dirs), CURRENT.parent / "snapshots")
+        self.assertEqual(next(dirs), CURRENT.parent / "defaults")
+        self.assertEqual(next(dirs), CURRENT.parent / "docs")
