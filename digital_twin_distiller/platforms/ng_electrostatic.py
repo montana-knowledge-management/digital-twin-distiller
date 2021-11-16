16fd94357101471535ed7efe62584ca261c8c014
gadokrisztian <gado.krisztian@montana.hu>
2021-11-16 13:32:09 +0100

    ngElec.: solver added

--- digital_twin_distiller/platforms/ng_electrostatic.py
+++ digital_twin_distiller/platforms/ng_electrostatic.py
@@ -146,8 +146,25 @@ class NgElectrostatics(Platform):
         self.newline(2)
 
     def ng_export_solving_steps(self):
-        self.comment("SOLVER")
-        self.comment("empty", 1)
+        self.comment("SOLVER", 1)
+        self.write(f'fes = H1(mesh, order=3, dirichlet=[1,2,3,4])')
+
+        self.write('u = fes.TrialFunction()')
+        self.write('v = fes.TestFunction()')
+        self.write('f = LinearForm(fes)')
+        self.write('f += 32 * (y*(1-y)+x*(1-x)) * v * dx')
+
+        self.write('a = BilinearForm(fes, symmetric=True)')
+        self.write('a += grad(u)*grad(v)*dx')
+
+        self.write('a.Assemble()')
+        self.write('f.Assemble()')
+
+        self.write('gfu = GridFunction(fes)')
+        self.write('gfu.vec.data = a.mat.Inverse(fes.FreeDofs(), inverse="sparsecholesky") * f.vec')
+
+
+        self.write('Draw (gfu)')
         
         self.newline(2)
 
