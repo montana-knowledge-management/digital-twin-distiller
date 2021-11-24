de1c9105adc09bd894b0093ffb6c8aa877ac0358
gadokrisztian <gado.krisztian@montana.hu>
2021-11-24 11:09:23 +0100

    T35: platforms added

--- applications/Team35/model.py
+++ applications/Team35/model.py
@@ -1,31 +1,50 @@
-from digital_twin_distiller.boundaries import DirichletBoundaryCondition
-from digital_twin_distiller.material import Material
-from digital_twin_distiller.metadata import FemmMetadata
-from digital_twin_distiller.model import BaseModel
-from digital_twin_distiller.modelpaths import ModelDir
-from digital_twin_distiller.platforms.femm import Femm
-from digital_twin_distiller.snapshot import Snapshot
+from digital_twin_distiller import DirichletBoundaryCondition, NeumannBoundaryCondition
+from digital_twin_distiller import Material
+from digital_twin_distiller import BaseModel
+from digital_twin_distiller import ModelDir
+from digital_twin_distiller import Agros2D, Femm
+from digital_twin_distiller import Agros2DMetadata, FemmMetadata
+from digital_twin_distiller import Snapshot
+from digital_twin_distiller import ModelPiece
+import numpy as np
 
 ModelDir.set_base(__file__)
 
 class Team35(BaseModel):
     """docstring for Team35"""
-    def __init__(self, **kwargs):
+    def __init__(self, X:list, **kwargs):
+        assert len(X) == 10
+        self.X = X.copy()
+
         super(Team35, self).__init__(**kwargs)
         self._init_directories()
 
     def setup_solver(self):
         femm_metadata = FemmMetadata()
         femm_metadata.problem_type = "magnetic"
-        femm_metadata.coordinate_type = "planar"
+        femm_metadata.coordinate_type = "axisymmetric"
         femm_metadata.file_script_name = self.file_solver_script
         femm_metadata.file_metrics_name = self.file_solution
         femm_metadata.unit = "millimeters"
-        femm_metadata.smartmesh = True
-        femm_metadata.depth = 1000
+        femm_metadata.smartmesh = False
 
-        self.platform = Femm(femm_metadata)
-        self.snapshot = Snapshot(self.platform)
+        agros_metadata = Agros2DMetadata()
+        agros_metadata.file_script_name = self.file_solver_script
+        agros_metadata.file_metrics_name = self.file_solution
+        agros_metadata.problem_type = "magnetic"
+        agros_metadata.coordinate_type = "axisymmetric"
+        agros_metadata.analysis_type = "steadystate"
+        agros_metadata.unit = 1e-3
+        agros_metadata.nb_refinements = 0
+        agros_metadata.adaptivity = "hp-adaptivity"
+        agros_metadata.polyorder = 2
+        agros_metadata.adaptivity_tol = 0.001
+
+        platform_femm = Femm(femm_metadata)
+        platform_agros = Agros2D(agros_metadata)
+
+        platform = platform_femm
+        self.snapshot = Snapshot(platform)
 
     def define_materials(self):
         air = Material('air')
