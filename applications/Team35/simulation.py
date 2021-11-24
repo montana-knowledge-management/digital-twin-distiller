37190c4ac069036ce345accf739c4f0fe1d8861a
gadokrisztian <gado.krisztian@montana.hu>
2021-11-24 10:46:03 +0100

    initial team35 model created

---
+++ applications/Team35/simulation.py
@@ -0,0 +1,28 @@
+from multiprocessing import Pool
+
+from model import Team35
+
+from digital_twin_distiller.modelpaths import ModelDir
+from digital_twin_distiller.server import Server
+from digital_twin_distiller.simulationproject import sim
+
+
+def execute_model(model: Team35):
+    result = model(timeout=2000, cleanup=True)
+    return result
+
+
+@sim.register('default')
+def default_simulation(model, modelparams, simparams, miscparams):
+    return "Hello World!"
+
+if __name__ == "__main__":
+
+    ModelDir.set_base(__file__)
+
+    # set the model for the simulation
+    sim.set_model(Team35)
+
+    model = Server(sim)
+    # model.build_docs()
+    model.run()
