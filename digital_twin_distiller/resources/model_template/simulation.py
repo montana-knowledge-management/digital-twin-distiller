19a2d307ea46f698580e4d8c5919ec660626920c
Tamas Orosz <oroszt@montana.hu>
2021-12-03 14:06:40 +0100

    server refactored in the template

--- digital_twin_distiller/resources/model_template/simulation.py
+++ digital_twin_distiller/resources/model_template/simulation.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     # set the model for the simulation
     sim.set_model(${name})
 
-    model = Server(sim)
+    model = Encapsulator(sim)
     model.build_docs()
     mounts()
     model.run()
