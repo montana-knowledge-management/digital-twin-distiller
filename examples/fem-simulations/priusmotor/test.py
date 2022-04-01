80ba9c7fb5c971e5a1fc74a50d81db3d4524bd08
Sevirekon <katona.mihaly@edu.bme.hu>
2022-04-01 17:25:16 +0200

    Figures

--- examples/fem-simulations/priusmotor/test.py
+++ examples/fem-simulations/priusmotor/test.py
@@ -6,25 +6,27 @@ from digital_twin_distiller import Node
 
 range_b0 = 0
 range_b1 = -60
-nsteps_b = 2
+nsteps_b = 61
 
 range_c0 = 100
 range_c1 = 250
-nsteps_c = 2
+nsteps_c = 4
 
-iterlist = []
+iterlist = [[], [], []]
 for i in range(nsteps_c):
-    for j in range(2):
+    for j in range(451):
         range_a0 = 0 + j/10
         range_a1 = 15 + j/10
-        nsteps_a = 2
+        nsteps_a = nsteps_b
 
         range_a = linspace(range_a0, range_a1, nsteps_a)
         range_b = linspace(range_b0, range_b1, nsteps_b)
         range_c = linspace(range_c0, range_c1, nsteps_c)
 
         for k in range(nsteps_b):
-            iterlist.append([round(range_a[k], 3), round(range_b[k], 3), range_c[i]])
+            iterlist[0].append(round(range_a[k], 3))
+            iterlist[1].append(round(range_b[k], 3))
+            iterlist[2].append(round(range_c[i], 3))
 
 
 print(iterlist)
