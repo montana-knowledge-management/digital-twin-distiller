b220fc4911214870fa6ecabc32bd56bec0b706b7
csanyig <csanyi.gergely@montana.hu>
2022-05-20 12:01:09 +0200

    custom input validation added

--- digital_twin_distiller/encapsulator.py
+++ digital_twin_distiller/encapsulator.py
@@ -7,27 +7,13 @@ from pathlib import Path
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from importlib_resources import files
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, ValidationError
 
-from digital_twin_distiller.ml_project import MachineLearningProject
 from digital_twin_distiller.modelpaths import ModelDir
-from digital_twin_distiller.simulationproject import SimulationProject
-
-
-class InputJsonML(BaseModel):
-    """
-    Class for validating the input sent to the /process endpoint for MachineLearningProject.
-    """
-
-    text: str
-
-    # Setting for keeping the additional keys in the input json intact
-    class Config:
-        extra = Extra.allow
 
 
 class InputJsonSim(BaseModel):
@@ -111,16 +97,25 @@ async def process_sim(item: InputJsonSim):
 
 
 @app.post("/process_ml", include_in_schema=True, tags=["process_ml"])
-async def process_ml(item: InputJsonML):
+async def process_ml(item: dict, response: Response):
     """
     Endpoint for performing the project.run() method on data sent for the API in JSON format.
     The endpoint performs automatic input validation via the Item class.
     """
-    data = json.loads(item.json())
-    if data:
-        app.project.add_single_input(data)
-        app.project.run()
-    return app.project.get_single_output()
+    if item:
+        try:
+            app.project.validate(item)
+        except ValidationError as e:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {
+                "status": "failed",
+                "error": e.__class__.__name__,
+                "detail": e.errors(),
+            }
+        else:
+            app.project.add_single_input(item)
+            app.project.run()
+            return app.project.get_single_output()
 
 
 @app.get("/ping", include_in_schema=True, tags=["ping"])
